a284575f6571d1ef4aeb6bebd69f918190cdf8d3
Nathan Keller <keller.nathan@gmail.com>
2019-10-09 10:58:58 -0600

    wrote method for rooms_in_cat but can't get room to shovel in :(

--- lib/house.rb
+++ lib/house.rb
@@ -13,23 +13,31 @@ class House
     @rooms << room
   end
 
+  def rooms_in_category(category)
+    cat_array = []
+    @rooms.each do |room|
+      if room.category == category
+        cat_array << room
+      end
+    end
+    cat_array
+  end
+
 end
 
-require './lib/room'
-require './lib/house'
-house = House.new("$400000", "123 sugar lane")
-room_1 = Room.new(:bedroom, 10, 13)
-room_2 = Room.new(:bedroom, 11, 15)
-room_3 = Room.new(:living_room, 25, 15)
-room_4 = Room.new(:basement, 30, 41)
-house.add_room(room_1)
-house.add_room(room_2)
-house.add_room(room_3)
-house.add_room(room_4)
+# require './lib/room'
+# require './lib/house'
+# house = House.new("$400000", "123 sugar lane")
+# room_1 = Room.new(:bedroom, 10, 13)
+# room_2 = Room.new(:bedroom, 11, 15)
+# room_3 = Room.new(:living_room, 25, 15)
+# room_4 = Room.new(:basement, 30, 41)
+# house.add_room(room_1)
+# house.add_room(room_2)
+# house.add_room(room_3)
+# house.add_room(room_4)
 # house.rooms_from_category(:bedroom)
-# #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
-#
-# pry(main)> house.rooms_from_category(:basement)
+# house.rooms_from_category(:basement)
 # #=> [#<Room:0x00007fccd297dc30...>]
 #
 # pry(main)> house.area
