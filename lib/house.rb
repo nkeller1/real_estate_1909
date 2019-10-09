cc616e035f0a8f5ef5ffaae6dd50d4ba9855aea1
Nathan Keller <keller.nathan@gmail.com>
2019-10-09 10:38:22 -0600

    stuck on adding a room to house (house.add_room(room_1)

--- lib/house.rb
+++ lib/house.rb
@@ -2,11 +2,13 @@ require 'pry'
 
 class House
   attr_reader :price, :address, :rooms
+  attr_accessor :house
 
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
+    @house = house
   end
 
   def add_room(room)
@@ -15,36 +17,18 @@ class House
 
 end
 
-pry(main)> require './lib/room'
-# #=> true
-#
-# pry(main)> require './lib/house'
-# #=> true
-#
-# pry(main)> house = House.new("$400000", "123 sugar lane")
-# #=> #<House:0x00007fccd30375f8...>
-#
-# pry(main)> room_1 = Room.new(:bedroom, 10, 13)
-# #=> #<Room:0x00007fccd29b5720...>
-#
-# pry(main)> room_2 = Room.new(:bedroom, 11, 15)
-# #=> #<Room:0x00007fccd2985f48...>
-#
-# pry(main)> room_3 = Room.new(:living_room, 25, 15)
-# #=> #<Room:0x00007fccd383c2d0...>
-#
-# pry(main)> room_4 = Room.new(:basement, 30, 41)
-# #=> #<Room:0x00007fccd297dc30...>
-#
-# pry(main)> house.add_room(room_1)
-#
-# pry(main)> house.add_room(room_2)
-#
-# pry(main)> house.add_room(room_3)
-#
-# pry(main)> house.add_room(room_4)
-#
-# pry(main)> house.rooms_from_category(:bedroom)
+require './lib/room'
+require './lib/house'
+House.new("$400000", "123 sugar lane")
+room_1 = Room.new(:bedroom, 10, 13)
+room_2 = Room.new(:bedroom, 11, 15)
+room_3 = Room.new(:living_room, 25, 15)
+room_4 = Room.new(:basement, 30, 41)
+house.add_room(room_1)
+# house.add_room(room_2)
+# house.add_room(room_3)
+# house.add_room(room_4)
+# house.rooms_from_category(:bedroom)
 # #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
 #
 # pry(main)> house.rooms_from_category(:basement)
