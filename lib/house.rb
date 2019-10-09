80cad379b9127cf3786000e7b23cbf32243579ec
Nathan Keller <keller.nathan@gmail.com>
2019-10-09 09:57:21 -0600

    remove room method and added in def initialize to house class

--- lib/house.rb
+++ lib/house.rb
@@ -1,17 +1,17 @@
 require 'pry'
 
 class House
-  attr_reader :price, :address
+  attr_reader :price, :address, :rooms
 
   def initialize(price, address)
     @price = price
     @address = address
+    @rooms = []
   end
 
-  def rooms
-    room = []
-    # room << 
-  end
+  # def rooms
+  #   # room <<
+  # end
 
 end
 
