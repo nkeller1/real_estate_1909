2159b30a66e753ead91dd5619467feacd3ebd3bd
Nathan Keller <keller.nathan@gmail.com>
2019-10-09 10:23:13 -0600

    starting iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -15,20 +15,40 @@ class House
 
 end
 
-
-# require './lib/room'
-# require './lib/house'
-# house = House.new("$400000", "123 sugar lane")
-# house.price
-# house.address
-# house.rooms
-# pry(main)> room_1 = Room.new(:bedroom, 10, 13)
-# pry(main)> room_2 = Room.new(:bedroom, 11, 15)
+pry(main)> require './lib/room'
+# #=> true
 #
-# #
-# # pry(main)> house.add_room(room_1)
-# #
-# # pry(main)> house.add_room(room_2)
-# #
-# pry(main)> house.rooms
+# pry(main)> require './lib/house'
+# #=> true
+#
+# pry(main)> house = House.new("$400000", "123 sugar lane")
+# #=> #<House:0x00007fccd30375f8...>
+#
+# pry(main)> room_1 = Room.new(:bedroom, 10, 13)
+# #=> #<Room:0x00007fccd29b5720...>
+#
+# pry(main)> room_2 = Room.new(:bedroom, 11, 15)
+# #=> #<Room:0x00007fccd2985f48...>
+#
+# pry(main)> room_3 = Room.new(:living_room, 25, 15)
+# #=> #<Room:0x00007fccd383c2d0...>
+#
+# pry(main)> room_4 = Room.new(:basement, 30, 41)
+# #=> #<Room:0x00007fccd297dc30...>
+#
+# pry(main)> house.add_room(room_1)
+#
+# pry(main)> house.add_room(room_2)
+#
+# pry(main)> house.add_room(room_3)
+#
+# pry(main)> house.add_room(room_4)
+#
+# pry(main)> house.rooms_from_category(:bedroom)
 # #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
+#
+# pry(main)> house.rooms_from_category(:basement)
+# #=> [#<Room:0x00007fccd297dc30...>]
+#
+# pry(main)> house.area
+#=> 1900
