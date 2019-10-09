f68cc0f6a699099fd60c5bfa63ee58be69b3d517
Nathan Keller <keller.nathan@gmail.com>
2019-10-09 10:21:21 -0600

    finished house class

--- lib/house.rb
+++ lib/house.rb
@@ -16,24 +16,19 @@ class House
 end
 
 
-require './lib/room'
-require './lib/house'
-house = House.new("$400000", "123 sugar lane")
-house.price
-house.address
-house.rooms
-
-# #=> []
-#
+# require './lib/room'
+# require './lib/house'
+# house = House.new("$400000", "123 sugar lane")
+# house.price
+# house.address
+# house.rooms
 # pry(main)> room_1 = Room.new(:bedroom, 10, 13)
-# #=> #<Room:0x00007fccd29b5720...>
-#
 # pry(main)> room_2 = Room.new(:bedroom, 11, 15)
-# #=> #<Room:0x00007fccd2985f48...>
 #
-# pry(main)> house.add_room(room_1)
-#
-# pry(main)> house.add_room(room_2)
-#
+# #
+# # pry(main)> house.add_room(room_1)
+# #
+# # pry(main)> house.add_room(room_2)
+# #
 # pry(main)> house.rooms
 # #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
