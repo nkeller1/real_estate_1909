289a4b6daf69d239f052171c00904d3f99948222
Nathan Keller <keller.nathan@gmail.com>
2019-10-09 10:46:19 -0600

    my house variable in my pry session was no registering. on rooms_from_ca

--- lib/house.rb
+++ lib/house.rb
@@ -2,13 +2,11 @@ require 'pry'
 
 class House
   attr_reader :price, :address, :rooms
-  attr_accessor :house
 
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
-    @house = house
   end
 
   def add_room(room)
@@ -19,15 +17,15 @@ end
 
 require './lib/room'
 require './lib/house'
-House.new("$400000", "123 sugar lane")
+house = House.new("$400000", "123 sugar lane")
 room_1 = Room.new(:bedroom, 10, 13)
 room_2 = Room.new(:bedroom, 11, 15)
 room_3 = Room.new(:living_room, 25, 15)
 room_4 = Room.new(:basement, 30, 41)
 house.add_room(room_1)
-# house.add_room(room_2)
-# house.add_room(room_3)
-# house.add_room(room_4)
+house.add_room(room_2)
+house.add_room(room_3)
+house.add_room(room_4)
 # house.rooms_from_category(:bedroom)
 # #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
 #
